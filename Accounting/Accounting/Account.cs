b61ca6a9b69928c346132e3e3f0079a373b4138f
sergey <sergey.pyrkin@instatsport.com>
2019-05-20 16:50:27 +0300

    Каркасс

---
+++ Accounting/Accounting/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting
+{
+    public class Account
+    {
+
+        public string fio { get; set; }
+        public DateTime StDate { get; set; }
+        public int days { get; set; }
+        public List<DateTime> usedDates = new List<DateTime>();
+    }
+}
